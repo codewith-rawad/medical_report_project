16615d4da1673d2829c8cbdedafd1842c7c6d861
rawad <rawad.rabie123@gmail.com>
2025-05-05 23:05:34 +0300

    add css to home page

--- medical_report_front/src/Pages/Home.jsx
+++ medical_report_front/src/Pages/Home.jsx
@@ -7,6 +7,7 @@ import Background from '../Components/Background';
 import Back1 from '../assets/background.jpg';
 import Back2 from '../assets/background2.jpg';
 import Back3 from '../assets/background3.jpg';
+import robot from "../assets/robot.webp"
 const Home = () => {
     const images=[Back1,Back2,Back3]
   const [showLogin, setShowLogin] = useState(false);
@@ -14,6 +15,12 @@ const Home = () => {
 
   return (
     <div className="all">
+      <div className="two">
+      <img  className='robot' src={robot} alt="robot image" />
+      <span>This web application leverages the power of Artificial Intelligence to automatically generate accurate medical reports from X-ray images.
+      By integrating advanced deep learning models, the system can analyze medical images, detect anomalies, and produce clear, professional diagnostic reports — streamlining the medical workflow and assisting healthcare professionals in decision-making.</span>
+      </div>
+   
           <Navbar 
         onLoginClick={() => setShowLogin(true)} 
         onSignupClick={() => setShowSignUp(true)} 
@@ -27,6 +34,8 @@ const Home = () => {
       {showSignUp && <SignUp onClose={() => setShowSignUp(false)} />}
 
  <Background images={images}/>
+
+
     </div>
     
     
